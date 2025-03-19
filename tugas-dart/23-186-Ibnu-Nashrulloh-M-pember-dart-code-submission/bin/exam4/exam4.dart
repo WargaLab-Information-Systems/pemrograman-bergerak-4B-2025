eba76533de3b51d4e5e8c6b6812f10baa60fef64
unbii <ibnu.nashrulloh.m27@gmail.com>
2025-03-19 12:56:54 +0700

    23_186_KAKROSYID_TUGAS_DART

--- tugas-dart/23-186-Ibnu-Nashrulloh-M-pember-dart-code-submission/bin/exam4/exam4.dart
+++ tugas-dart/23-186-Ibnu-Nashrulloh-M-pember-dart-code-submission/bin/exam4/exam4.dart
@@ -4,27 +4,27 @@ class PraktikanStudent {
 
   PraktikanStudent(this.fullName, this.age);
 
+
   int incrementAge() {
     // TODO 1
-
-    return 0;
-
+    age = age + 1;
+    return age;
     // End of TODO 1
   }
 
   Future<String> getStudentInfo() {
     // TODO 2
-
-    return Future.value("");
-
+    return Future.delayed(
+      Duration(seconds: 3),
+      () => "Nama Lengkap: $fullName, Umur: $age tahun",
+    );
     // End of TODO 2
   }
 }
 
 dynamic createStudent() {
   // TODO 3
-
-  return null;
-
+  var praktikanStudent = PraktikanStudent("Ibnu Nashrulloh", 20);
+  return praktikanStudent;
   // End of TODO 3
 }
