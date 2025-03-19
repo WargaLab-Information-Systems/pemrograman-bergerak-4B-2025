f4bc5b410b101341675ecb39c55c417d76147adc
trinanda537 <trinandaindrayana7@gmail.com>
2025-03-19 13:33:12 +0700

    23_108_Trinanda_indrayana_kak_Rosyid_tugasDart

---
+++ tugas-dart/23_108-pember-dart-code-submission7/bin/exam4/exam4.dart
@@ -0,0 +1,28 @@
+class PraktikanStudent {
+  String fullName;
+  int age;
+
+  PraktikanStudent(this.fullName, this.age);
+
+  int incrementAge() {
+    // TODO 1
+    age += 1; 
+    return age; 
+    // End of TODO 1
+  }
+
+  Future<String> getStudentInfo() {
+    // TODO 2
+    return Future.delayed(Duration(seconds: 3), () {
+      return "Nama Lengkap: $fullName, Umur: $age tahun";
+    });
+    // End of TODO 2
+  }
+}
+
+dynamic createStudent() {
+  // TODO 3
+  var praktikanStudent = PraktikanStudent("Trinanda Indrayana",20,);
+  return praktikanStudent;
+  // End of TODO 3
+}
