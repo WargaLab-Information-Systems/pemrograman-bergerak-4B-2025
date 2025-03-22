1fe7e9d0fa1ae3304d778da42d9bf3e39a171a78
ammnaaaaaa <amanatulmukarromah232@gmail.com>
2025-03-22 13:57:13 +0700

    AmanatulMukarromah_230441100010_Exam_KakJabbar

--- tugas-dart/pember-dart-code-submission/bin/exam4/exam4.dart
+++ tugas-dart/pember-dart-code-submission/bin/exam4/exam4.dart
@@ -6,8 +6,7 @@ class PraktikanStudent {
 
   int incrementAge() {
     // TODO 1
-
-    return 0;
+    return age + 1;
 
     // End of TODO 1
   }
@@ -15,7 +14,9 @@ class PraktikanStudent {
   Future<String> getStudentInfo() {
     // TODO 2
 
-    return Future.value("");
+    return Future.delayed(Duration(seconds: 3), () {
+      return "Nama Lengkap: ${fullName}, Umur: ${age} tahun";
+    });
 
     // End of TODO 2
   }
@@ -23,8 +24,8 @@ class PraktikanStudent {
 
 dynamic createStudent() {
   // TODO 3
-
-  return null;
+  PraktikanStudent Gue = PraktikanStudent("Akbar", 17);
+  return Gue;
 
   // End of TODO 3
 }
